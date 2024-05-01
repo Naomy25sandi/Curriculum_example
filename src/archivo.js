5e242067bbe54905d813b058748803c27c3e3b36
Naomy25sandi <naomysandi25@gmail.com>
2024-05-01 15:37:07 -0600

    agregue funciones de js

--- src/archivo.js
+++ src/archivo.js
@@ -8,7 +8,15 @@ function boton1() {
 let boton2 = document.getElementById("boton2").onclick = function () {
     click2()
 }
-function () {
-    alert("Sou una persona activa con iniciativa")
+function boton2() {
+    alert("Soy una persona activa con iniciativa")
     
+}
+
+let boton3 = document.getElementById(boton3).onclick = function () {
+    click3()
+}
+
+let boton4 = document.getElementById(boton4).onclick = function () {
+    click4
 }
